e54c9a30b1f5f4f90b6bf3f93562cee1d48c6f5b
Joachim Kliemann <joachim.kliemann@googlemail.com>
2025-08-23 20:24:24 +0200

    add 11ty blog

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "orangegrau",
+	url: "https://orangegrau.de/",
+	language: "en",
+	description: "I am writing about my experiences as a frontend developer.",
+	author: {
+		name: "Joachim Kliemann",
+		email: "joachimkliemann@gmail.com",
+		url: "https://orangegrau.de/about-me/"
+	}
+}
